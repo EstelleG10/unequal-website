05c2f23db6f51b6426d1e41fa097e6e1da4ced2f
EstelleG10 <estelle.gerber@yale.edu>
2025-04-22 11:00:22 -0400

    brewing event updates

--- app/brewing_event/page.js
+++ app/brewing_event/page.js
@@ -79,58 +79,40 @@ export default function CookingEventPage() {
             {/* Event Description */}
             <div className="mt-12 max-w-7xl mx-auto text-gray-200 space-y-6 leading-relaxed text-xl">
 
-                <p><strong>“Nearly 4000 years ago, in what is now the land of Iraq, a song was composed in honor of the divine Ninkasi, goddess of a golden, delicious, nutritious drink that we love and enjoy to this day.”</strong></p>
+                <p><strong>“Nearly 4000 years ago, in what is now the land of Iraq, a song was composed in honor of the divine Ninkasi, goddess of a golden, delicious, nutritious drink that we love and enjoy to this day.” </strong></p>
 
                 <p>
-                    With this cheeky preface, Dr. Gojko Barjamovic of Yale’s Assyriology department introduced Dr. Tate Paulette of North Carolina State University to a crowd of roughly 80 people in an elegantly-adorned corridor of the Yale Peabody Museum.
-                    Here, Dr. Paulette delivered a lively guest lecture summarizing his book <em>In the Land of Ninkasi: A History of Beer in Ancient Mesopotamia</em>. As the world’s foremost expert on ancient brewing techniques and what he calls “gastro-politics,”
-                    Paulette deftly wove archaeological evidence, ancient texts, and the formidable task of their interpretation into a story-driven account of Mesopotamian brewing practices.
+                    With this preface, co-instructor of NELC120 Unequal, Gojko Barjamovic of Yale’s Department of Near Eastern Languages and Cultures, introduced Dr.Tate Paulette of North Carolina State University to a full house of 160 guests in the elegantly adorned Great Hall of the Yale Peabody Museum. Here, Paulette delivered a lively guest lecture based on his 2024 book In the Land of Ninkasi: A History of Beer in Ancient Mesopotamia. Paulette’s book is the first text to pull together the evidence on ancient brewing in a monograph form. As the world’s foremost expert on ancient beer brewing techniques and what he calls “gastro-politics,” Paulette deftly wove archaeological evidence, ancient texts, and the formidable task of their interpretation into a story-driven account of Mesopotamian brewing practices.
                 </p>
 
                 <p>
-                    “What we have in 3000 B.C. Mesopotamia is evidence of the world’s first great beer culture,” Paulette told the audience. Research in recent decades, particularly in the arena of trace residue analysis, has given us a fragmentary knowledge of brewing practices in earlier cultures.
-                    What’s remarkable about Paulette’s work in third-millennium BC Mesopotamia, by contrast, is that “We suddenly have a wealth of evidence to work with... This is the first time we can really see a beer culture in its full complexity.”
-                    This picture, he says, comes to us in the forms of archaeological, artistic, and written evidence.
+                    “What we have in 3000 B.C. Mesopotamia is evidence of the world’s first great beer culture,” Paulette told the audience. Research in recent decades, particularly in the arena of trace residue analysis, has given us a fragmentary knowledge of brewing practices in earlier cultures. What’s remarkable about Paulette’s work in third-millennium BC Mesopotamia, by contrast, is that “We suddenly have a wealth of evidence to work with ... This is the first time we can really see a beer culture in its full complexity.” This picture, he says, comes to us in the forms of archaeological, artistic, and written evidence.
                 </p>
 
                 <p>
-                    One of the more amusing pieces of archaeological evidence Paulette shared with the audience was a “cylinder seal”: used as something akin to a personal signature, cylinder seals were ornately-decorated pieces which, when rolled over wet clay, left an imprint of an image unique to its possessor.
-                    One seal in particular seemed to leave little room for misinterpretation: its imprint revealed a man drinking beer through a straw, simultaneously laid out over a woman, in what one hopes was a passionate, drunken love affair.
-                    “In some ways,” Paulette noted with a grin, “drinking culture hasn’t changed a bit since ancient times.”
+                    One of the more amusing pieces of archaeological evidence Paulette shared with the audience was a cylinder seal (an ornately-decorated object which, when rolled over wet clay, left an imprint of an image unique to its owner—akin to a signature) that seemed to leave little room for misinterpretation: its imprint shows a man drinking beer through a straw, simultaneously laid out over a woman, in what one hopes was a passionate, drunken love affair. “In some ways,” Paulette noted with a grin, “drinking culture hasn’t changed a bit since ancient times.”
                 </p>
 
                 <p>
-                    Etiquette aside, Mesopotamian drinking culture was just as socially significant as it was economically indispensable.
-                    Thousands of proto-cuneiform tablets have been recovered which detail administrative accounts of commercial logistics, like grain stores, delivery receipts, brewing recipes, and tavern locations.
-                    They were remarkably specific: on one side of a tablet you might find an inventory of types and quantities of beer; on the other, instructions for how much should be delivered to various locations.
-                    Other tablets and artifacts evinced myriad other ways ancient Mesopotamians related to beer, including protective rituals, prayers and hymns to the goddess Ninkasi, drinking songs,
-                    and literary accounts of drunkenness varying from “slightly buzzed” to what can only be described as “dangerously wasted.”
+                    Etiquette aside, Mesopotamian drinking culture was as socially significant as it was economically indispensable. Thousands of cuneiform tablets have been recovered which detail administrative accounts of commercial logistics, like grain stores, delivery receipts, brewing recipes, and tavern locations. They were remarkably specific: you might find an inventory of types and quantities of beer and instructions for how much should be delivered to various locations. Other evidence includes protective rituals; prayers and hymns to Ninkasi, goddess of brewing; drinking songs; and narrative accounts of drunkenness varying from “slightly buzzed” to what can only be described as “dangerously wasted.”
                 </p>
 
                 <p>
-                    As soon as the lecture concluded, people eagerly swarmed the adjacent bar for a chance to sample the brews they had been promised: as an accompaniment to Dr. Paulette’s talk,
-                    Yale Biology postdoc Vanessa Todorow and Dr. Christian Mahl generously offered their time serving samples of two beers <em>[DESCRIBE]</em>.
-                    Vanessa and Dr. Mahl brewed these beers specially for this event, following recipes developed by an inter-departmental effort to re-create these ancient beverages.
-                    In addition, Yale’s Near Eastern Languages and Civilizations department had kindly arranged a vast array of light-fare charcuterie for guests to enjoy with their libations.
-                    Rather than brave the line snaking outside the banquet hall, I decided to wait it out and eavesdrop on a group of people waiting for a chance to ask Dr. Paulette further questions.
-                    By the time I did reach the bar, the charcuterie tables had been reduced to piles of wilted lettuce and grape stems—signs of an evening well-enjoyed by the many guests in attendance—
-                    but I was lucky that neither of the two kegs I’d hoped to sample had run dry.
-                    One <em>[describe appearance and taste]</em>, the other <em>[describe appearance and taste – pink sweet, brown sour, what yeast strains?]</em>.
+                    After the lecture and a lively Q&A, guests eagerly swarmed the bar set up among the towering dinosaurs in Burke Hall for a chance to sample ancient brews. As an accompaniment to Dr. Paulette’s talk, Yale School of Medicine researchers Vanessa Todorow, Zane Johnson and Christian Mahl—who have spent the semester as auxiliary course instructors in NELC120 in charge of the brewing team—offered samples of two Mesopotamian beers that they brewed specially for this event, following recipes developed in an inter-departmental effort to re-create these ancient beverages.
                 </p>
 
                 <p>
-                    As both an accessible public educational experience and a hands-on enrichment of the brewing team’s course contributions, the event was a fascinating, funny, and charming experience for all in attendance.
-                    Learning about Paulette’s research, while simultaneously sharing the process of our archaeological and brewing efforts in this class,
-                    brought us closer to understanding the significance of Mesopotamian beer culture than we ever could have hoped to achieve in the classroom alone.
+                    Rather than brave the line for the food tables snaking outside the banquet hall, I decided to wait it out and eavesdrop on a group of people while waiting for a chance to ask Dr. Paulette further questions. By the time I did reach the bar, the charcuterie tables had been reduced to piles of wilted lettuce and grape stems—signs of an evening well-enjoyed by the many guests in attendance—but I was lucky that neither of the two kegs I’d hoped to sample had run dry. The first beer, simply called “sweet beer,” had a low alcohol content with what Todorow called “a bready profile, with some fruity and mildly sour notes.” The other, named the “good beer,” had a stronger, 5-6% ABV content, with a more acidic, citrusy profile, accompanied by herbal and peppery undertones.
+                </p>
+
+                <p>
+                    As both an accessible educational experience open to the New Haven public and a hands-on enrichment of the brewing team’s course contributions, the event was a fascinating, fun, and charming experience for all in attendance. Learning about Dr. Paulette’s research, while simultaneously incorporating his insights with the process of our archaeological and brewing efforts in class, brought us closer to understanding the significance of Mesopotamian beer culture than we could have achieved in the classroom alone.
                 </p>
 
                 <p className="text-lg text-gray-400 italic">
-                    This lecture was generously organized with funding from Yale’s Department of Near Eastern Languages and Civilizations, Departments of Anthropology, the Yale Ancient Pharmacology Program,
-                    The Babylonian Collection, and the Franke Program in Science and the Humanities.
+                    The lecture and event was generously organized with funding from the Yale Peabody Museum, Yale’s Department of Near Eastern Languages and Civilizations, Department of Anthropology, the Yale Ancient Pharmacology Program, The Yale Babylonian Collection, and the Franke Program in Science and the Humanities.
                 </p>
             </div>
-
-
         </div>
 
     );
--- app/trace_residue/page.js
+++ app/trace_residue/page.js
@@ -65,16 +65,50 @@ export default function CookingEventPage() {
             {/* Title & Event Date */}
             <div className="text-center">
                 <h1 className="text-7xl font-extrabold drop-shadow-md">Trace Residue</h1>
-                <h2 className="text-3xl font-extrabold drop-shadow-md">--------</h2>
-                <p className="text-lg text-orange-600 mt-2">----.</p>
-                <p className="text-md text-grey-300 font-semibold mt-1">---</p>
-                <p className="text-md text-grey-300 font-semibold mt-1">---- </p>
             </div>
 
-            {/* Event Picture  */}
-            <div className="mt-8 flex justify-center">
-                <img src="/beer.jpeg" alt="picture" className="" />
+            {/* Event Picture */}
+            <div className="mt-8 max-w-4xl mx-auto flex justify-center">
+                <img src="/trsquad.jpeg" alt="picture" className="" />
             </div>
+
+            {/* Trace Residue Description */}
+            <div className="mt-10 max-w-4xl mx-auto text-lg space-y-6 leading-relaxed">
+                <p>As I walked into an unassuming laboratory in the Yale Peabody Museum’s basement, I noticed two things: first, the distinctive whiff of acetone; second, the beaming smile on Dr. Andrew Koh’s face. Koh was preparing his students for a novel experiment in Trace Residue Analysis: a process of extracting specific chemical compounds (also known as “biomarkers”) from artifacts to detect residues of ancient substances, including foodstuffs, resins, or beverages, typically using methods such as Gas Chromatography–Mass Spectrometry (GC-MS).</p>
+
+                <p>As the founder of the ARCHEM project in 2003, Koh has specialized in the development of a field he calls archaeochemistry. He believes that current methodologies of artifact analysis are falling short of their potential, and that a more interdisciplinary approach—combining chemistry, archaeology, ethnobotany and history—could reveal more than strict chemical analysis alone. As Koh puts it: “We're trying to make this archaeochemistry, not just pure chemistry."</p>
+
+                <p>Besides the limiting nature of a strictly chemistry-driven approach to trace residue analysis, it typically comes with other challenges. For instance, the traditional protocol used by chemists—which requires the extraction and pulverization of a small sample from the artifact itself by drilling a small hole and collecting the remnants—is inherently destructive. This can put chemists at odds with museum conservators who wish to limit damage to their collections. Koh and his team have found a brilliant solution to this problem: they have developed a protocol in which artifacts can be “washed” with solvents that extract trace residues without damaging the artifacts themselves.</p>
+
+                {/* Event Picture */}
+                <div className="mt-8 flex justify-center">
+                    <img src="/trpot.jpeg" alt="picture" className="" />
+                </div>
+
+                <p>As a means-testing project, Koh et al. are using this non-destructive protocol to examine residues of two suspected beer jars. Roughly 1,800 years old and hailing from the Roman period, these vessels have excellent provenance–a term used to describe vessels with a well-documented origin and custodial history. They have never been cleaned or displayed and, as relatively untouched specimens, they are ideal for trace residue analysis. If these vessels did indeed contain organic compounds indicating the presence of beer, they could serve as “type vessels” for identifying beer vessels all over the world.</p>
+
+                <p>In order to confirm the presence of beer residues in these artifacts, Koh, in conjunction with the Brewing Team from NELC120, needed to create “control” vessels. Using specially crafted jars from local ceramicist Maishe Dickman, the NELC120 Brewing Team helped develop historically appropriate recipes which were then brewed in Maishe’s jars, themselves copies of brewing kits excavated at a number of North Mesopotamian sites dating to the 23rd–22nd century BCE. Using his “wash” trace residue protocol, the trace residue team intends to compare the remnants in the ancient and contemporary jars in an effort to identify a residue which, if found in both vessels, can be used as a biomarker to confirm the presence of beer in artifacts found all over the world.</p>
+
+                <div className="mt-10 bg-black/50 backdrop-blur-md p-6 rounded-lg shadow-lg">
+                    <p className="text-lg text-gray-300 mt-2">Watch the wash in action!</p>
+
+                    <div className="mt-4 flex justify-center">
+                        <div className="w-full max-w-3xl aspect-video rounded-lg overflow-hidden shadow-lg">
+                            <iframe
+                                className="w-full h-full"
+                                src="https://www.youtube.com/embed/07ca_bVZaCc"
+                                title="Event Video"
+                                frameBorder="0"
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                allowFullScreen
+                            ></iframe>
+                        </div>
+                    </div>
+                </div>
+
+                <p>This interdisciplinary approach to trace residue analysis has exciting potential to teach us about the lives of ancient people. Besides the matter of identifying trace residue for Mesopotamian beer, this work contributes to a broader understanding of human history: food and drink can help us reconstruct fundamental aspects of the social lives of ancient humans, including their diets, economies, and social hierarchies.</p>
+            </div>
+
         </div>
 
     );
