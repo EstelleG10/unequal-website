1ef2d93e567e48c28c90ec046994aa25af9e7039
EstelleG10 <estelle.gerber@yale.edu>
2025-04-24 00:46:44 -0400

    museum group fixes

--- app/museum/page.js
+++ app/museum/page.js
@@ -96,28 +96,70 @@ export default function CookingEventPage() {
                         </p>
                     </div>
 
+                    <header className="text-center space-y-2">
+                        <h1 className="text-6xl text-orange-600 font-bold tracking-tight">
+                            <span className="">Breaking Down The Curatorial Process </span>
+                        </h1>
+                    </header>
+
+                    <div>
+                        <h3 className="text-2xl font-semibold text-[#E2A96D]">1. Picking A Theme </h3>
+                        <p className="max-w-[90rem] mx-auto" >
+                            As we drafted our exhibition themes, it became clear that even in brainstorming, these concepts had to be grounded in material culture. Thus, we immediately began selecting objects from the collections to help visualize potential themes. It was immediately evident that some stories of inequality were easier to tell through the collections than others. We realized that the narrative of our cases had to be succinct while still allowing us to represent multiple object types, scales, and regions. Thus, a sort of “let them eat cake” theme arose. By exploring inequality through food and drink, we are able to work with objects related to daily life, funerary culture, and elitism.
+                        </p>
+                    </div>
                     {/* Event image */}
                     <div className="mt-8 flex justify-center">
                         <img src="/concept.jpg" alt="Event Poster" className="w-97 h-95 max-w-xl rounded-lg shadow-lg" />
                     </div>
 
                     <div>
-                        <h3 className="text-2xl font-semibold text-orange-600">Object Selection</h3>
+                        <h3 className="text-2xl font-semibold text-[#E2A96D]">2. Choosing Objects </h3>
                         <p className="max-w-[90rem] mx-auto" >
-                            Through the process of selecting objects, the museum group quickly learned both how rewarding the process can be and how difficult it can be.
-                            Not only did all group members need to agree on the vision for the case but each object also had to be available and possible to be showcased in the provided case.
-                            For example, if it was being used for research, displayed elsewhere, required high security, or was not prepared to be displayed, then the group was unable to use the object.
-                            So, the members came up with lists of objects for each case, knowing that they might need to shift their plans depending on what was possible.
-                            While this first posed as a challenge, I found that it led to deeper conversation over which objects to select.
-                            It also meant that they drafted a longer list of objects that they were interested in, leading to more options when it was time to design case layout.
+                            Our goal was to select a visually diverse set of objects that illustrate inequality in daily life. For instance, we chose to display two cups: one made of redware and one made of ceramic with ornate designs. We found that the ornate ceramic cup came from a tomb, while the redware cup was discovered buried between two village houses. Though the archeological contexts differ, these objects still demonstrate inequality. Having a proper tomb was typically associated with elite status in Ancient Egypt, this decorative ceramic cup was likely a valuable ceremonial object, not meant for practical use. The redware cup, on the other hand, was functional and common. We believe that displaying objects like these can paint a comprehensive picture of inequality through the culture surrounding food and feast.
                         </p>
                     </div>
 
-                    {/* Event image */}
+                    {/* Event image
                     <div className="mt-8 flex justify-center">
                         <img src="/breakdown.jpg" alt="Event Poster" className="w-97 h-95 max-w-xl rounded-lg shadow-lg" />
+                    </div> */}
+
+                    <div>
+                        <h3 className="text-2xl font-semibold text-[#E2A96D]">3. Adjusting Object Selection </h3>
+                        <p className="max-w-[90rem] mx-auto" >
+                            After we felt confident about our object list, we formally requested items through the Peabody. We quickly ran into a few issues, which forced us to replace several objects. The problems were primarily related to the size constraints within the cases, in addition to some conservation concerns. Custom mounts were not possible given our limited time frame, so particularly large, fragile, or non-freestanding objects were automatically eliminated. For instance, a fragmented stela we planned on being the visual anchor in the case was shockingly roughly 140 lbs and thus too heavy to be exhibited. We collaborated with Peabody conservators, curators, liaisons, and many other fantastic people to find replacement objects. We then constructed a more feasible object list that still fit our narrative.
+                        </p>
                     </div>
 
+                    <div>
+                        <h3 className="text-2xl font-semibold text-[#E2A96D]">4. Designing The Cases </h3>
+                        <p className="max-w-[90rem] mx-auto" >
+                            Once the final item list had been compiled, we began to formalize the layout across the cases. We allocated one case to describe the making of the feast and the other to the feast itself, separating objects accordingly. To better visualize the layouts of each case, various members of our group created various digital models of the space. This included illustrated renditions of the cases in addition to 3-D models accomplished through CAD. We wanted the display to look balanced while still mixing object size, type, and color. After a few drafts and ideas for posters on the top shelves, we shared our designs with the museum team at the Peabody.
+
+                        </p>
+                    </div>
+
+                    <div>
+                        <h3 className="text-2xl font-semibold text-[#E2A96D]">5. Coordinating Object Delivery and Preparation </h3>
+                        <p className="max-w-[90rem] mx-auto" >
+                            Though many of the objects on our list were conveniently stored at the Peabody, many require additional steps before their exhibition. For example, one culinary tablet is on display, so we had to commission a 3D-printed replica. Another piece, “Fragment of an Offering Table”, was reported to have a “salt problem” and needed cleaning. Other objects, such as a large amphora, are still on pallets at West Campus, further pushing our exhibition date. After accounting for cleaning, transportation, and installation, we expect the exhibit to be complete by the end of June.
+                        </p>
+                    </div>
+                    <div>
+                        <h3 className="text-2xl font-semibold text-[#E2A96D]">6. Drafting a Narrative </h3>
+                        <p className="max-w-[90rem] mx-auto" >
+                            Crafting the purpose of our exhibit involved creating a compelling narrative that was both cogent and meaningful. For each case—“Feast Preparation” and “Feasting Itself”—we distilled intricate historical relationships and power dynamics into focused introductory labels that abided by the museum’s 20–25 word “big idea” and 60–65 word supporting text format. We prioritized clarity, emotional engagement, and thematic cohesion. For “Feast Preparation,” we highlighted the labor and power behind every elaborate feast. Our words needed to connect unassuming objects into a unified narrative, enabling visitors to see that ancient feasts were not merely a matter of celebration or nourishment but of power, reinforcing economic and ritual order.
+                        </p>
+                    </div>
+
+                    <div>
+                        <h3 className="text-2xl font-semibold text-[#E2A96D]">7. Writing Labels </h3>
+                        <p className="max-w-[90rem] mx-auto" >
+                            Like much of this process, writing labels for our objects proved a bigger challenge than initially anticipated. We gleaned most information through the LUX catalog, but our labels were made robust by utilizing resources such as transliteration books to help decipher tablets. The biggest challenge was prioritizing information that would appeal to the general public, not just a member of this class. This meant thinking carefully about how much context to give, how to phrase things clearly, and how to make sure each label supported the bigger story we’re trying to tell.
+                        </p>
+                    </div>
+                    {/* 
                     <div>
                         <h3 className="text-2xl font-semibold text-orange-600">Case Layout</h3>
                         <p className="max-w-[90rem] mx-auto" >
@@ -127,33 +169,8 @@ export default function CookingEventPage() {
                             In regard to case layout, it meant that people were skilled with CAD (Computer Aided Design), Photoshop, and drawing.
                             With all of these skills, the case layouts below were created.
                         </p>
-                    </div>
+                    </div> */}
                 </div>
-
-
-
-
-                {/* Images of Case Design */}
-                <div className="mt-8 bg-black/50 backdrop-blur-md p-6 rounded-lg shadow-lg">
-                    <div className="flex flex-wrap justify-center gap-2">
-                        <img
-                            src="/harper.jpg"
-                            alt="Example"
-                            className="w-74 h-60 rounded-lg object-cover"
-                        />
-                        <img
-                            src="/CAD.jpeg"
-                            alt="CAD layout"
-                            className="w-74 h-60 rounded-lg object-cover"
-                        />
-                        <img
-                            src="/drawing.jpg"
-                            alt="Drawing layout"
-                            className="w-74 h-60 rounded-lg object-cover"
-                        />
-                    </div>
-                </div>
-
             </div>
         </div>
     );
